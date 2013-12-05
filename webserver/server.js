d78fb5bfd77956c270e24381bcad7402954d0630
Changbin Park <changbin.park@lge.com>
2013-12-05 10:18:16 +0900

    Add node.js base simple webserver
    Execute webserver/server.js to use it

---
+++ webserver/server.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+var connect = require('connect');
+
+const listenPort=8000;
+var wwwRoot=__dirname + ".."";
+
+connect.createServer(
+	connect.static(wwwRoot)
+).listen(listenPort);
+
+console.log("Simple Webserver is listening");
+console.log("Home Directory : " + wwwRoot);
+console.log("Port           : " + listenPort);
